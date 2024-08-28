b8cffe6cf26c4755717eca25fe09415004bfcd31
Jan Šuráň <suran.orgpad@gmail.com>
2024-08-28 23:54:53 +0200

    Bump version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jansuran03/task-scheduler "1.0.0"
+(defproject org.clojars.jansuran03/task-scheduler "1.0.1"
   :description "A Clojure library designed for asynchronous scheduling of tasks."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
